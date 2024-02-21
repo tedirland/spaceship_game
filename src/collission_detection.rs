6c99af7c5daf6a097072963f5dadffdddf02adc2
tedirland <irlandth@gmail.com>
2024-02-20 18:39:31 -0600

    Refactor collision detection to use eventwriter and event readers

--- src/collission_detection.rs
+++ src/collission_detection.rs
@@ -1,6 +1,13 @@
+use std::ptr::read;
+
 use bevy::{prelude::*, utils::HashMap};
 
-use crate::{asteroids::Asteroid, schedule::InGameSet, spaceship::Spaceship};
+use crate::{
+    asteroids::Asteroid,
+    health::Health,
+    schedule::InGameSet,
+    spaceship::{Spaceship, SpaceshipMissile},
+};
 
 #[derive(Component, Debug)]
 pub struct Collider {
@@ -54,11 +61,17 @@ impl Plugin for CollisionDetectionPlugin {
         .add_systems(
             Update,
             (
-                handle_collisions::<Asteroid>,
-                handle_collisions::<Spaceship>,
+                (
+                    handle_collisions::<Asteroid>,
+                    handle_collisions::<Spaceship>,
+                    handle_collisions::<SpaceshipMissile>,
+                ),
+                apply_collison_damage,
             )
-                .in_set(InGameSet::DespawnEntities),
-        );
+                .chain()
+                .in_set(InGameSet::EntityUpdates),
+        )
+        .add_event::<CollisionEvent>();
     }
 }
 
@@ -94,7 +107,7 @@ fn collision_detection(mut query: Query<(Entity, &GlobalTransform, &mut Collider
 }
 
 fn handle_collisions<T: Component>(
-    mut commands: Commands,
+    mut collison_event_writer: EventWriter<CollisionEvent>,
     query: Query<(Entity, &Collider), With<T>>,
 ) {
     for (entity, collider) in query.iter() {
@@ -103,8 +116,29 @@ fn handle_collisions<T: Component>(
             if query.get(collided_entity).is_ok() {
                 continue;
             }
-            // Despawn the entity.
-            commands.entity(entity).despawn_recursive();
+            // Send collision event
+            collison_event_writer.send(CollisionEvent::new(entity, collided_entity));
         }
+    }
+}
+
+pub fn apply_collison_damage(
+    mut collision_event_reader: EventReader<CollisionEvent>,
+    mut health_query: Query<&mut Health>,
+    collision_damage_query: Query<&CollisionDamage>,
+) {
+    for &CollisionEvent {
+        entity,
+        collided_entity,
+    } in collision_event_reader.read()
+    {
+        let Ok(mut health) = health_query.get_mut(entity) else {
+            continue;
+        };
+        let Ok(collision_damage) = collision_damage_query.get(collided_entity) else {
+            continue;
+        };
+
+        health.value -= collision_damage.amount;
     }
 }
--- src/health.rs
+++ src/health.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 #[derive(Component, Debug)]
 pub struct Health {
-    value: f32,
+    pub value: f32,
 }
 
 impl Health {
